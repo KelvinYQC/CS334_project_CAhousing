caa5bf1cd6068c01098a8941fddee8a1c054465a
Kelvin <yiqing.cheng@emory.edu>
2021-11-14 13:33:32 -0500

    randomSearch

--- ModelTuning.py
+++ ModelTuning.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, GradientBoostingClassifier
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.tree import DecisionTreeRegressor
 import xgboost as xgb
 
@@ -22,14 +22,16 @@ parameterResult = list()
 dt = DecisionTreeRegressor()
 hyperparameters_dt = [{"splitter": ["best", "random"],
                        #'max_depth': range(1, 80, 5),
-                        'max_depth': range(1, 10, 5),
+                       'max_depth': range(1, 10, 5),
                        'min_samples_leaf': range(1, 700, 40),
                        "max_features": ["auto", "log2", "sqrt", None]}]
 
-dt_gridSearch = GridSearchCV(dt, hyperparameters_dt, cv=10)
+# dt_gridSearch = GridSearchCV(dt, hyperparameters_dt, cv=10)
+dt_gridSearch = RandomizedSearchCV(dt, hyperparameters_dt, n_iter=288, n_jobs=-1, cv=10, random_state=1)
 best_model_dt = dt_gridSearch.fit(X_train, y_train)
 print("decision tree tuned result")
 print(best_model_dt.best_estimator_)
+# %%
 parameterResult.append((dt_gridSearch.__class__.__name__, best_model_dt.best_estimator_))
 
 turnedmax_depth = best_model_dt.best_estimator_.get_params()['max_depth']
@@ -47,17 +49,20 @@ tunedResult.append((clf_dt.__class__.__name__, classifier_r2_score_dt))
 # %%
 # RandomForestRegressor
 rd = RandomForestRegressor()
-hyperparameters_rd = [{# 'bootstrap': [True, False],
+hyperparameters_rd = [{'bootstrap': [True, False],
                        'max_depth': [10, 20, 30, 40, 50, None],
-                       # 'max_features': ['auto', 'sqrt'],
+                       'max_features': ['auto', 'sqrt'],
                        'min_samples_leaf': range(1, 500, 100),
                        'n_estimators': [100, 200, 500, 800]}
                       ]
 
-clf = GridSearchCV(rd, hyperparameters_rd, cv=10)
+# clf = GridSearchCV(rd, hyperparameters_rd, cv=10)
+clf = RandomizedSearchCV(rd, hyperparameters_rd, n_iter=500, n_jobs=-1, cv=10, random_state=1)
+
 best_model = clf.fit(X_train, y_train)
 print("random forest tuned result")
 print(best_model.best_estimator_)
+# %%
 parameterResult.append((clf.__class__.__name__, best_model.best_estimator_))
 
 turnedmax_depth = best_model.best_estimator_.get_params()['max_depth']
@@ -84,17 +89,18 @@ tunedResult.append((clf_rd.__class__.__name__, classifier_r2_score_rd))
 # gradient boost
 parameters = {
     "loss": ["deviance"],
-    "learning_rate": [0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2],
-    "min_samples_split": np.linspace(0.1, 0.5, 5),
-    "min_samples_leaf": np.linspace(0.1, 0.5, 5),
+    "learning_rate": [0.0001,0.001, 0.01, 0.05, 0.1],
+    # "min_samples_split": np.linspace(0.1, 0.5, 5),
+    "min_samples_leaf": [1,10,50],
     "max_depth": [3, 5, 8],
     "max_features": ["log2", "sqrt"],
     "criterion": ["friedman_mse", "mae"],
-    "subsample": [0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0],
+    # "subsample": [0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0],
     'n_estimators': [10, 50, 100, 200, 500],
 }
 # #passing the scoring function in the GridSearchCV
-clf = GridSearchCV(GradientBoostingClassifier(), parameters, refit=False, cv=10)
+# clf = GridSearchCV(GradientBoostingClassifier(), parameters, refit=False, cv=10)
+clf = RandomizedSearchCV(GradientBoostingClassifier, parameters, n_iter=500, n_jobs=-1, cv=10, random_state=1)
 best_model = clf.fit(X_train, y_train)
 print("gradient boost tuned result")
 print(best_model.best_estimator_)
@@ -132,9 +138,9 @@ tunedResult.append((clf_gb.__class__.__name__, classifier_r2_score_gb))
 parameters = {
     'learning_rate': [0.001, 0.01, 0.3, 0.5, 0.7, 1],
     'max_depth': [3, 5, 7, 10, 20, 30, 50],
-    'min_child_weight': [1, 3, 5, 7, 10, 20, 50, 100],
+    # 'min_child_weight': [1, 3, 5, 7, 10, 20, 50, 100],
     'subsample': [0.3, 0.5, 0.7],
-    'colsample_bytree': [0.5, 0.7],
+    # 'colsample_bytree': [0.5, 0.7],
     'n_estimators': [100, 200, 500],
     'objective': ['reg:squarederror', 'reg:squaredlogerror']
 }
